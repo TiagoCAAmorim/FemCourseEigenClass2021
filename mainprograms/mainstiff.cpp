cb165859836c2a4ad12caf1169822b92e1bcfad4
TiagoCAAmorim <tiago.ca.amorim@gmail.com>
2023-09-21 15:10:40 -0300

    Created mainstiff.cpp

---
+++ mainprograms/mainstiff.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+// #include "VTKGeoMesh.h"
+// #include "Geom1d.h"
+#include "CompElementTemplate.h"
+using namespace std;
+
+
+void StiffFunc();
+
+int main() {
+    StiffFunc();
+    return 0;
+}
+
+void StiffFunc(){
+
+
+    CompElementTemplate<Shape1d> cel(index, &);
+}
