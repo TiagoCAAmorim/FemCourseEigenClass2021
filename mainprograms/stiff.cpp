7d79c03116cf925863c5dd95f9d050d7866f3a7c
TiagoCAAmorim <tiago.ca.amorim@gmail.com>
2023-09-22 17:57:00 -0300

    Linear and quadratic StiffMat calculation tests

--- mainprograms/stiff.cpp
+++ mainprograms/stiff.cpp
@@ -22,43 +22,77 @@ int main() {
 
 void CallStiffFunc(){
 
-    std::cout << "'Horizontal beam'\n";
-    //Nodes in the geometry mesh
+    std::cout << "Setting up geometry\n";
     const int dim = 2;
-    const int numnodes = 2;
+    const int numnodes = 3;
     GeoMesh gmesh;
     gmesh.SetDimension(dim);
     gmesh.SetNumNodes(numnodes);
-    GeoNode gnod0, gnod1;
-    VecDouble co0(dim), co1(dim);
+    GeoNode gnod0, gnod1, gnod2;
+    VecDouble co0(dim), co1(dim), co2(dim);
     co0 << 0., 0.;
     co1 << 4., 0.;
+    co2 << 0., 4.;
     gnod0.SetCo(co0);
     gnod1.SetCo(co1);
+    gnod2.SetCo(co2);
     gmesh.Node(0) = gnod0;
     gmesh.Node(1) = gnod1;
+    gmesh.Node(2) = gnod2;
     
-    //New 1D element between nodes 0 and 1
+    std::cout << "Setting up material\n";
     int materialid = 0;
-    VecInt nodeindices(2);
-    nodeindices << 0,1;
-    int index = 0;
-    GeoElementTemplate<Geom1d> geo(nodeindices,materialid,&gmesh,index);
+    MatrixDouble perm(3,3);
+    perm.setIdentity();
+    perm(1,1) = 2.;
+    Poisson poi(materialid, perm);
 
-    //New computational mesh based on geometry mesh, with linear functions (?)
+    std::cout << "Setting up 'Horizontal beam': #0\n";
+    VecInt nodeindices_h(2);
+    nodeindices_h << 0,1;
+    int index_h = 0;
+    GeoElementTemplate<Geom1d> geo_h(nodeindices_h,materialid,&gmesh,index_h);
+
+    std::cout << "Setting up 'Vertical beam': #1\n";
+    VecInt nodeindices_v(2);
+    nodeindices_v << 0,2;
+    int index_v = 1;
+    GeoElementTemplate<Geom1d> geo_v(nodeindices_v,materialid,&gmesh,index_v);
+
+    std::cout << "Building computational mesh: linear\n";
     int order = 1;
     CompMesh cmesh(&gmesh);
     cmesh.SetDefaultOrder(order);
-    
-    // Criando material
-    MatrixDouble perm(3,3);
-    perm.setIdentity();
-    perm(2,2) = 2.;
-    Poisson poi(materialid, perm);
     cmesh.SetMathStatement(materialid, &poi);
     
-    CompElementTemplate<Shape1d> cel(index,&cmesh,&geo);
     MatrixDouble ek(order+1,order+1),ef(order+1,1);
-    cel.CalcStiff(ek, ef);
+
+    std::cout << "Stiffness matrix for 'Horizontal beam'\n";
+    CompElementTemplate<Shape1d> cel_h(index_h,&cmesh,&geo_h);
+    cel_h.CalcStiff(ek, ef);
     cout << ek << endl;
+
+    std::cout << "Stiffness matrix for 'Vertical beam'\n";
+    CompElementTemplate<Shape1d> cel_v(index_v,&cmesh,&geo_v);
+    cel_v.CalcStiff(ek, ef);
+    cout << ek << endl;
+
+
+    std::cout << "Building computational mesh: quadratic\n";
+    order = 2;
+    CompMesh cmesh_quad(&gmesh);
+    cmesh_quad.SetDefaultOrder(order);
+    cmesh_quad.SetMathStatement(materialid, &poi);
+    
+    MatrixDouble ek_quad(order+1,order+1),ef_quad(order+1,1);
+
+    std::cout << "Stiffness matrix for 'Horizontal beam'\n";
+    CompElementTemplate<Shape1d> cel_h_quad(index_h,&cmesh_quad,&geo_h);
+    cel_h_quad.CalcStiff(ek_quad, ef_quad);
+    cout << ek_quad << endl;
+
+    std::cout << "Stiffness matrix for 'Vertical beam'\n";
+    CompElementTemplate<Shape1d> cel_v_quad(index_v,&cmesh_quad,&geo_v);
+    cel_v_quad.CalcStiff(ek_quad, ef_quad);
+    cout << ek_quad << endl;
 }
