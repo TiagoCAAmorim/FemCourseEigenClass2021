765a704e080ec4f9e5bb04d7d48c3e49a8c34291
TiagoCAAmorim <tiago.ca.amorim@gmail.com>
2023-12-06 22:18:13 -0300

    Working code for exercise 03

--- mainprograms/ExerciseThree.cpp
+++ mainprograms/ExerciseThree.cpp
@@ -40,16 +40,10 @@ void test2D(std::string filename, std::string functionname, const std::function<
     proj.setZero();
     val1.setZero();
     val2.setZero();
-    L2Projection *bc_left   = new L2Projection(0,2,proj,val1,val2);
-    L2Projection *bc_right  = new L2Projection(0,3,proj,val1,val2);
-    L2Projection *bc_bottom = new L2Projection(0,4,proj,val1,val2);
-    L2Projection *bc_top    = new L2Projection(0,5,proj,val1,val2);
-    bc_left->SetExactSolution(exact);
-    bc_right->SetExactSolution(exact);
-    bc_bottom->SetExactSolution(exact);
-    bc_top->SetExactSolution(exact);
+    L2Projection *bc   = new L2Projection(0,2,proj,val1,val2);
+    bc->SetExactSolution(exact);
 
-    std::vector<MathStatement *> mathvec = {0,mat1,bc_left, bc_right, bc_bottom, bc_top};
+    std::vector<MathStatement *> mathvec = {0,mat1,bc};
     cmesh.SetMathVec(mathvec);
     cmesh.SetDefaultOrder(order);
     cmesh.AutoBuild();  // Monta os DOFs e as equacoes associadas.
@@ -74,125 +68,88 @@ void test2D(std::string filename, std::string functionname, const std::function<
 }
 
 void mutipletests(std::string functionname, const std::function<void(const VecDouble &co, VecDouble &result)> &force, const std::function<void(const VecDouble &loc, VecDouble &result, MatrixDouble &deriv)> &exact){
-    std::string foldername = "D:/FemCourseEigenClass2021/ex02/";
-    std::vector<std::string> filenames = {"Mesh1Quad", "Mesh4Quad", "Mesh2Tri", "Mesh8Tri"};
+    std::string foldername = "D:/FemCourseEigenClass2021/ex03/";
+    std::vector<std::string> filenames = {"MalhaAnelQuad_0", "MalhaAnelTria_0"};
+    int max_refine = 5; // (1 to 5)
     int max_order = 2; // (1 or 2)
 
     for (int i=0; i<filenames.size(); i++){
         for (int j=0; j<max_order; j++){
-            std::cout << "" << std::endl;
-            std::cout << "##### " << filenames[i] << " #####" << std::endl;
-            std::cout << "#####   exact function: " << functionname << " #####" << std::endl;
-            std::cout << "#####   order: " << (j+1) << " #####" << std::endl;
-            test2D(foldername+filenames[i], functionname, force, exact, j+1);
+            for (int k=0; k<max_refine; k++){
+                std::cout << "" << std::endl;
+                std::cout << "##### " << filenames[i] << (k+1) << " #####" << std::endl;
+                std::cout << "#####   exact function: " << functionname << " #####" << std::endl;
+                std::cout << "#####   order: " << (j+1) << " #####" << std::endl;
+                test2D(foldername+filenames[i]+std::to_string(k+1), functionname, force, exact, j+1);
+            }
         }
     }
 }
-
-void mutipletests2(std::string functionname, const std::function<void(const VecDouble &co, VecDouble &result)> &force, const std::function<void(const VecDouble &loc, VecDouble &result, MatrixDouble &deriv)> &exact){
-    std::string foldername = "D:/FemCourseEigenClass2021/ex02/";
-    std::vector<std::string> filenames = {"Mesh1Quad", "Mesh4Quad", "Mesh16Quad", "Mesh64Quad", "Mesh256Quad", "Mesh2Tri", "Mesh8Tri", "Mesh32Tri", "Mesh128Tri", "Mesh512Tri"};
-    int max_order = 2; // (1 or 2)
-
-    for (int i=0; i<filenames.size(); i++){
-        for (int j=0; j<max_order; j++){
-            std::cout << "" << std::endl;
-            std::cout << "##### " << filenames[i] << " #####" << std::endl;
-            std::cout << "#####   exact function: " << functionname << " #####" << std::endl;
-            std::cout << "#####   order: " << (j+1) << " #####" << std::endl;
-            test2D(foldername+filenames[i], functionname, force, exact, j+1);
-        }
-    }
-}
-
 
 int main(){
     SetDebug(false);
 
-    auto exact1 = [](const VecDouble &x, VecDouble &val, MatrixDouble &deriv)
-    {
-        val[0] = 1;
-        deriv(0,0) = 0;
-        deriv(1,0) = 0;
-    };
-    auto force1 = [](const VecDouble &x, VecDouble &res)
-    {
-        res[0] = 0;
-    };
-    mutipletests("unity", force1, exact1);
-
-    auto exact2 = [](const VecDouble &x, VecDouble &val, MatrixDouble &deriv)
+    auto exactX = [](const VecDouble &x, VecDouble &val, MatrixDouble &deriv)
     {
         val[0] = x[0];
-        deriv(0,0) = 1;
-        deriv(1,0) = 0;
+        deriv(0,0) = 1.;
+        deriv(1,0) = 0.;
     };
-    auto force2 = [](const VecDouble &x, VecDouble &res)
+    auto forceX = [](const VecDouble &x, VecDouble &res)
     {
         res[0] = 0;
     };
-    mutipletests("x", force2, exact2);
+    // mutipletests("x", forceX, exactX);
 
-    auto exact3 = [](const VecDouble &x, VecDouble &val, MatrixDouble &deriv)
-    {
-        val[0] = x[1];
-        deriv(0,0) = 0;
-        deriv(1,0) = 1;
-    };
-    auto force3 = [](const VecDouble &x, VecDouble &res)
-    {
-        res[0] = 0;
-    };
-    mutipletests("y", force3, exact3);
-
-    auto exact4 = [](const VecDouble &x, VecDouble &val, MatrixDouble &deriv)
+    auto exactXY = [](const VecDouble &x, VecDouble &val, MatrixDouble &deriv)
     {
         val[0] = x[0]*x[1];
         deriv(0,0) = x[1];
         deriv(1,0) = x[0];
     };
-    auto force4 = [](const VecDouble &x, VecDouble &res)
+    auto forceXY = [](const VecDouble &x, VecDouble &res)
     {
         res[0] = 0;
     };
-    mutipletests("xy", force4, exact4);
+    // mutipletests("xy", forceXY, exactXY);
 
-    auto exact5 = [](const VecDouble &x, VecDouble &val, MatrixDouble &deriv)
+    auto exactX2 = [](const VecDouble &x, VecDouble &val, MatrixDouble &deriv)
     {
         val[0] = x[0]*x[0];
         deriv(0,0) = 2*x[0];
         deriv(1,0) = 0.;
     };
-    auto force5 = [](const VecDouble &x, VecDouble &res)
+    auto forceX2 = [](const VecDouble &x, VecDouble &res)
     {
         res[0] = -2;
     };
-    mutipletests("x2", force5, exact5);
+    // mutipletests("x2", forceX2, exactX2);
 
-    auto exact6 = [](const VecDouble &x, VecDouble &val, MatrixDouble &deriv)
-    {
-        val[0] = x[1]*x[1];
-        deriv(0,0) = 0.;
-        deriv(1,0) = 2*x[1];
-    };
-    auto force6 = [](const VecDouble &x, VecDouble &res)
-    {
-        res[0] = -2;
-    };
-    mutipletests("y2", force6, exact6);
-
-    auto exact7 = [](const VecDouble &x, VecDouble &val, MatrixDouble &deriv)
+    auto exactX2Y2 = [](const VecDouble &x, VecDouble &val, MatrixDouble &deriv)
     {
         val[0] = x[0]*x[0]*x[1]*x[1];
         deriv(0,0) = 2*x[0]*x[1]*x[1];
         deriv(1,0) = 2*x[0]*x[0]*x[1];
     };
-    auto force7 = [](const VecDouble &x, VecDouble &res)
+    auto forceX2Y2 = [](const VecDouble &x, VecDouble &res)
     {
         res[0] = -2*x[0]*x[0]-2*x[1]*x[1];
     };
-    mutipletests("x2y2", force7, exact7);
-    mutipletests2("x2y2", force7, exact7);
+    // mutipletests("x2y2", forceX2Y2, exactX2Y2);
+
+    auto exact = [](const VecDouble &x, VecDouble &val, MatrixDouble &deriv)
+    {
+        double pi = 3.1415926535897932384626433832795028842;
+        val[0] = sin(3. * pi * x[0]) * sin(pi * x[1]);
+        deriv(0,0) = 3. * pi * cos(3. * pi * x[0]) * sin(pi * x[1]);
+        deriv(1,0) = pi * sin(3. * pi * x[0]) * cos(pi * x[1]);
+    };
+    auto force = [](const VecDouble &x, VecDouble &res)
+    {
+        double pi = 3.1415926535897932384626433832795028842;
+        res[0] = 10. * pi * pi * sin(3. * pi * x[0]) * sin(pi * x[1]);
+    };
+    mutipletests("Ex03", force, exact);
 
     return 0;
 }
